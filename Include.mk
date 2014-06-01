e8246a8d47cc73d1aee01e5d39b463a6235382e8
Christopher Moore <christopher.e.moore@gmail.com>
2014-05-31 20:37:02 -0700

    update makefile

--- Include.mk
+++ Include.mk
@@ -1,6 +1,5 @@
 PARALLEL_PATH:=$(dir $(lastword $(MAKEFILE_LIST)))
 
 INCLUDE+=$(PARALLEL_PATH)include
-LIBPATHS+=$(PARALLEL_PATH)dist/$(PLATFORM)/$(BUILD)
-LIBS+=Parallel
+DYNAMIC_LIBS+=$(PARALLEL_PATH)dist/$(PLATFORM)/$(BUILD)/libParallel.dylib
 
