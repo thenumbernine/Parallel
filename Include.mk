42e90e6cf274e4ca9f362ebb138aa53caee29eef
Christopher E. Moore <christopher.e.moore@gmail.com>
2015-09-08 16:41:16 +0300

    removing the old line

--- Include.mk
+++ Include.mk
@@ -1,6 +1,5 @@
 PARALLEL_PATH:=$(dir $(lastword $(MAKEFILE_LIST)))
 
 INCLUDE+=$(PARALLEL_PATH)include
-#DYNAMIC_LIBS+=$(PARALLEL_PATH)dist/$(PLATFORM)/$(BUILD)/libParallel.dylib
 DYNAMIC_LIBS+=$(PARALLEL_PATH)dist/$(PLATFORM)/$(BUILD)/libParallel$(LIB_SUFFIX)
 
